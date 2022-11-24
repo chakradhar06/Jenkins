de2c97802959e185cd5d1af037375e5b9e8f092d
Chakradhar Vanarasi <78954910+chakradhar06@users.noreply.github.com>
2022-11-24 23:56:37 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,32 @@
+import unittest
+from fibonacci import Fibonacci
+
+class TestFibonacci(unittest.TestCase):
+    # correct test case
+    def test_fib1(self):
+        result = Fibonacci(6)
+        self.assertEqual(result, 13)
+        
+    # correct test case
+    def test_fib1(self):
+        result = Fibonacci(7)
+        self.assertEqual(result, 21)
+    
+    # correct test case
+    def test_fib1(self):
+        result = Fibonacci(8)
+        self.assertEqual(result, 34)
+    
+    # wrong test case
+    def test_fib1(self):
+        result = Fibonacci(9)
+        self.assertEqual(result, 56)
+    
+    # wrong test case
+    def test_fib1(self):
+        result = Fibonacci(10)
+        self.assertEqual(result, 88)
+        
+        
+if __name__ == '__main__':
+    unittest.main()
